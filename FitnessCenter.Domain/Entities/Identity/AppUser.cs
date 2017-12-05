0cb51e31e8ef34ff466c95461e6bb6db0eb536f0
shanzra <shanzra@commvault.com>
2017-12-05 10:48:55 -0500

    Add project files.

---
+++ FitnessCenter.Domain/Entities/Identity/AppUser.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FitnessCenter.Domain.Entities.Identity
+{
+        public class AppUser : IdentityUser
+        {
+       
+            [Required]
+            public string FirstName { get; set; }
+            [Required]
+            public string LastName { get; set; }
+
+            public virtual ICollection<Registration> Registration { get; set; }
+        }
+}
---
+++ FitnessCenter.Domain/Entities/Registration.cs
@@ -0,0 +1,30 @@
+﻿using FitnessCenter.Domain.Entities.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace FitnessCenter.Domain.Entities
+{
+    public class Registration
+    {
+        [Key]
+        public int RegistrationID { get; set; }        
+        public int ScheduleID { get; set; }
+        [ForeignKey("AppUser")]
+        public string UserID { get; set; }
+        public int WaitNumber { get; set; }
+        public DateTime DateTime { get; set; }
+        public RegisrtationStatus Status { get; set; }
+        [Required]
+        public virtual Schedule Schedule { get; set; }
+        public virtual AppUser AppUser { get; set; }
+    }
+
+    public enum RegisrtationStatus
+    {
+        Confirmed,
+        Waiting
+    }
+}
