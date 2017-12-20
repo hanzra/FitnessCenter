605d694cc175fd573657692d39acc371efcc35b8
shanzra <shanzra@commvault.com>
2017-12-20 15:15:29 -0500

    Structure for adding users and roles

---
+++ FitnessCenter.Domain/ViewModel/Account/UserViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace FitnessCenter.Domain.ViewModel.Account
+{
+    public class UserViewModel
+    {
+        public string Id { get; set; }        
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Display(Name = "Confirm Password")]
+        [DataType(DataType.Password)]
+        public string ConfirmPassword { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public List<SelectListItem> Roles { get; set; }
+        [Display(Name = "Role")]
+        public string RoleId { get; set; }
+    }
+}
---
+++ FitnessCenter.Web/Controllers/AccountController.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Identity;
+using FitnessCenter.Domain.Entities.Identity;
+using FitnessCenter.Domain.ViewModel.Account;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace FitnessCenter.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly SignInManager<AppUser> _signInManager;
+        private readonly UserManager<AppUser> _userManager;
+        private readonly RoleManager<AppRole> _roleManager;
+
+        public AccountController(UserManager<AppUser> userManager, RoleManager<AppRole> roleManager, SignInManager<AppUser> signInManager)
+        {
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _signInManager = signInManager;
+        }
+
+        #region user management
+
+        [HttpGet]
+        public IActionResult Users()
+        {
+            List<UserListViewModel> model = new List<UserListViewModel>();
+            model = _userManager.Users.Select(u => new UserListViewModel
+            {
+                Id = u.Id,
+                FirstName = u.FirstName,
+                LastName = u.FirstName,
+                Email = u.Email
+            }).ToList();
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult AddUser()
+        {
+            UserViewModel model = new UserViewModel();
+            model.Roles = _roleManager.Roles.Select(r => new SelectListItem
+            {
+                Text = r.Name,
+                Value = r.Id
+            }).ToList();
+            return PartialView("_AddUser", model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> AddUser(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                AppUser user = new AppUser
+                {
+                    FirstName = model.FirstName,
+                    LastName = model.LastName,
+                    UserName = model.Email,
+                    Email = model.Email
+                };
+                IdentityResult result = await _userManager.CreateAsync(user, model.Password);
+                if (result.Succeeded)
+                {
+                    AppRole applicationRole = await _roleManager.FindByIdAsync(model.RoleId);
+                    if (applicationRole != null)
+                    {
+                        IdentityResult roleResult = await _userManager.AddToRoleAsync(user, applicationRole.Name);
+                        if (roleResult.Succeeded)
+                        {
+                            return RedirectToAction("Users");
+                        }
+                    }
+                }
+            }
+            return View(model);
+        }
+
+        #endregion 
+
+        #region role management
+
+        [HttpGet]
+        public IActionResult Roles()
+        {
+            List<AppRoleListViewModel> model = new List<AppRoleListViewModel>();
+            model = _roleManager.Roles.Select(r => new AppRoleListViewModel
+            {
+                RoleName = r.Name,
+                Id = r.Id,
+                Description = r.NormalizedName,                
+            }).ToList();
+            return View(model);
+        }
+        [HttpGet]
+        public async Task<IActionResult> AddEditApplicationRole(string id)
+        {
+            AppRoleViewModel model = new AppRoleViewModel();
+            if (!String.IsNullOrEmpty(id))
+            {
+                AppRole applicationRole = await _roleManager.FindByIdAsync(id);
+                if (applicationRole != null)
+                {
+                    model.Id = applicationRole.Id;
+                    model.RoleName = applicationRole.Name;
+                    model.Description = applicationRole.NormalizedName;
+                }
+            }
+            return PartialView("_AddEditApplicationRole", model);
+        }
+        [HttpPost]
+        public async Task<IActionResult> AddEditApplicationRole(string id, AppRoleViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                bool isExist = !String.IsNullOrEmpty(id);
+                AppRole applicationRole = isExist ? await _roleManager.FindByIdAsync(id) : new AppRole {};
+
+                applicationRole.Name = model.RoleName;
+                applicationRole.NormalizedName = model.Description;                
+                IdentityResult roleRuslt = isExist ? await _roleManager.UpdateAsync(applicationRole)
+                                                    : await _roleManager.CreateAsync(applicationRole);
+                if (roleRuslt.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+            }
+            return View(model);
+        }
+        #endregion
+    }
+}
